dedf24d1616d434094709722a7c5b2b71d1755f1
swwho96 <swwho96@gmail.com>
2022-08-10 18:30:36 +0900

    1667. Fix Names in a Table (concat, substring)

---
+++ leetcode/FixNameInATable.sql
@@ -0,0 +1,8 @@
+'''
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
+'''
+SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(substring(name,2))) AS name
+FROM Users
+ORDER BY user_id

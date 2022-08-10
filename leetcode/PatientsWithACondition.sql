8921b2ac78e48211ddf60c0008e87e8127ed9940
swwho96 <swwho96@gmail.com>
2022-08-10 23:43:00 +0900

    1527. Patients With a Condition (LIKE)

---
+++ leetcode/PatientsWithACondition.sql
@@ -0,0 +1,10 @@
+'''
+Write an SQL query to report the patient_id, patient_name all conditions of patients who have Type I Diabetes.
+Type I Diabetes always starts with DIAB1 prefix
+
+Return the result table in any order.
+'''
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE "DIAB1%" OR conditions LIKE "% DIAB1%"

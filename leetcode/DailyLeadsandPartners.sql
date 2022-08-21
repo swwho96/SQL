9202ab9f7be62553ee28e32757a63d48f37ec932
swwho96 <swwho96@gmail.com>
2022-08-22 00:35:27 +0900

    1693. Daily Leads and Partners

---
+++ leetcode/DailyLeadsandPartners.sql
@@ -0,0 +1,7 @@
+'''
+Write an SQL query that will, for each date_id and make_name, return the number of distinct lead_id's and distinct partner_id's.
+'''
+
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) as unique_leads, COUNT(DISTINCT partner_id) as unique_partners
+FROM DailySales
+GROUP BY make_name, date_id
